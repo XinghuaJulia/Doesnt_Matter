1481c9fd3acd3706704436229bf28e8fad8007bc
UselessTrash <colleteiscoco@gmail.com>
2024-05-26 14:55:56 +0800

    Implemented virtual pet skeleton.

--- Filler/App.js
+++ Filler/App.js
@@ -2,10 +2,11 @@ import 'react-native-url-polyfill/auto'
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import GptScreen from './app/HomeScreen'
 import TrashUploadScreen from './app/TrashUploadScreen'
 import GenerateTipsScreen from './app/GenerateTipsScreen'
-import LoginScreen from './app/HomeScreen'
+import LoginScreen from './app/LoginScreen'
+import GameScreen from './app/GameScreen'
+import HomeScreen from './app/HomeScreen'
 
 
 const Stack = createNativeStackNavigator()
@@ -14,10 +15,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="GptScreen" component={GptScreen} />
+        <Stack.Screen name="HomeSceeen" component={HomeScreen} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="TrashUploadScreen" component={TrashUploadScreen} />
-        <Stack.Screen name="GenerateTipsScreen" component={GenerateTipsScreen} />
+        <Stack.Screen name="GameScreen" component={GameScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
