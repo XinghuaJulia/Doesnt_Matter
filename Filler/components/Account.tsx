924b840b160641594cfba3eb07ca010ce10dcaf3
UselessTrash <colleteiscoco@gmail.com>
2024-06-16 15:49:08 +0800

    added weekly and daily reset of points for virtual pet

--- Filler/components/Account.tsx
+++ Filler/components/Account.tsx
@@ -4,8 +4,10 @@ import { StyleSheet, View, Alert, Text } from 'react-native'
 import { Button, Input } from '@rneui/themed'
 import { Session } from '@supabase/supabase-js'
 import Avatar from './Avatar'
-import { FlashList } from "@shopify/flash-list"
+import { FlashList } from '@shopify/flash-list'
 import { useNavigation } from '@react-navigation/native'
+import { daysAgo, pointsThisWeek, pointsToday } from './utils/helper'
+
 
 
 
@@ -16,6 +18,8 @@ export default function Account({ session }: { session: Session }) {
   const [avatarUrl, setAvatarUrl] = useState('')
   const [users, setUsers] = useState<{id: string}[]>([])
   const [points, setPoints] = useState(0)
+  const [points_week, setPointsWeek] = useState(0)
+  const [activity, setActivity] = useState("You haven't earned any points yet, start today!")
 
   const navigation = useNavigation()
 
@@ -37,7 +41,7 @@ export default function Account({ session }: { session: Session }) {
 
       const { data, error, status } = await supabase
         .from('profiles')
-        .select(`username, website, avatar_url, points`)
+        .select(`username, website, avatar_url, points, last_acitivity, points_week`)
         .eq('id', session?.user.id)
         .single()
       
@@ -50,6 +54,8 @@ export default function Account({ session }: { session: Session }) {
         setWebsite(data.website)
         setAvatarUrl(data.avatar_url)
         setPoints(data.points)
+        setActivity(data.last_acitivity)
+        setPointsWeek(data.points_week)
       }
     } catch (error) {
       if (error instanceof Error) {
@@ -95,21 +101,62 @@ export default function Account({ session }: { session: Session }) {
     }
   }
 
+  // async function initialisePoints() {
+  //   try {
+  //     const { data, error, status } = await supabase
+  //       .from('profiles')
+  //       .select(`points, last_activity, points_week`)
+  //       .eq('id', session?.user.id)
+  //       .single()
+
+  //     const tempPoints = data.points ? data.points : 0
+  //     const tempPointsWeek = data.points_week ? data.points_week : 0
+
+  //     const updates = {
+  //       id: session?.user.id,
+  //       points: tempPoints,
+  //       points_week: tempPointsWeek,
+  //     }
+
+  //     await supabase.from('profiles').upsert(updates)
+
+  //     if (error) {
+  //       throw error
+  //     }
+  //   } catch (error) {
+  //     if (error instanceof Error) {
+  //       Alert.alert(error.message)
+  //     }
+  //   } finally {
+  //     setLoading(false)
+  //   }
+  // }
+
   async function updatePoints() {
     try {
+
       const { data, error, status } = await supabase
         .from('profiles')
-        .select(`points`)
+        .select(`points, last_activity, points_week`)
         .eq('id', session?.user.id)
         .single()
 
+      const tempPoints = pointsToday(data.last_activity, data.points ? data.points : 0) + 1
+      const tempPointsWeek = pointsThisWeek(data.last_activity, data.points_week ? data.points_week : 0) + 1
+
       const updates = {
         id: session?.user.id,
-        points: data.points + 1,
+        points: tempPoints,
+        points_week: tempPointsWeek,
+        last_activity: new Date(),
       }
 
       await supabase.from('profiles').upsert(updates)
-      setPoints(data.points + 1)
+
+
+      setPoints(tempPoints)
+      setPointsWeek(tempPointsWeek)
+      setActivity(daysAgo(data.last_activity))
 
       if (error) {
         throw error
@@ -125,15 +172,6 @@ export default function Account({ session }: { session: Session }) {
 
   return (
     <View style={styles.container}>
-
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button
-          title={'add points'}
-          onPress={() => updatePoints()}
-          disabled={loading}
-        />
-      </View>
-      <Text>Your current points are: {points || 0}</Text>
 
       <View>
         <Avatar
@@ -163,17 +201,21 @@ export default function Account({ session }: { session: Session }) {
         />
       </View>
 
-      
+      <View>
+        <Button
+          title={'add points'}
+          onPress={() => updatePoints()}
+          disabled={loading}
+        />
+      </View>
+      <Text>Your points today: {points || 0}</Text>
+      <Text>Your points this week: {points_week || 0}</Text>
+      <Text>Last activity: {activity}</Text>
 
       <View style={styles.verticallySpaced}>
         <Button title="Sign Out" onPress={() => supabase.auth.signOut()} />
       </View>
 
-
-      
-
-
-      
     </View>
   )
 }
